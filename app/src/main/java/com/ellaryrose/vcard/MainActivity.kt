902b9ac5aa3ffce23afb16f4dde81822984d1a79
EllaryRose <ellaryrose93@gmail.com>
2019-07-28 13:56:26 -0700

    Initial commit
    Completed linear layout V Card

---
+++ app/src/main/java/com/ellaryrose/vcard/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ellaryrose.vcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout_vcard)
+    }
+}
---
+++ app/src/main/java/com/ellaryrose/vcard/features/LinearLayoutVCard.kt
@@ -0,0 +1,13 @@
+package com.ellaryrose.vcard.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ellaryrose.vcard.R
+
+class LinearLayoutVCard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout_vcard)
+    }
+}
